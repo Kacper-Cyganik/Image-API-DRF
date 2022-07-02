80e1e94dcd0914e594b26182406409be9006d559
Kacper <kacper-cyganik@o2.pl>
2022-07-02 19:23:38 +0200

    Create all_images view & html.

--- core/images/urls.py
+++ core/images/urls.py
@@ -1,9 +1,11 @@
 from re import template
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import ImageListView
 
 app_name = 'images'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='images/index.html')),
+    path('all/', ImageListView.as_view(), name='all-images'),
 ]
--- core/images/views.py
+++ core/images/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category, Image
 
-# Create your views here.
+class ImageListView(ListView):
+    model = Image
+    template_name = 'images/all_images.html'
+
